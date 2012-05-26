af47f3536e2b35c356c4c4555ec71a747ece94da
Scott L <schott12521@gmail.com>
2012-05-25 22:54:25 -0400

    This better fudgen work

---
+++ ScottsWorkspace/Chapter2/src/com/ScottL/Chapter2/ButtonActivity.java
@@ -0,0 +1,38 @@
+package com.ScottL.Chapter2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ButtonActivity extends Activity implements OnClickListener{
+    
+    EditText edittext;
+    Button button1;
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        final EditText edittext = (EditText) findViewById(R.id.edittext);
+        
+        button1.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Toast.makeText(getBaseContext() , edittext.getText(), 250).show();
+			}
+		});
+    }
+
+	public void onClick(View v) {
+		Intent intent = new Intent(ButtonActivity.this, TextFieldActivity.class);
+		intent.putExtra("text", edittext.getText().toString());
+		startActivity(intent);
+		
+	}
+}
---
+++ ScottsWorkspace/Chapter2/src/com/ScottL/Chapter2/TextFieldActivity.java
@@ -0,0 +1,22 @@
+package com.ScottL.Chapter2;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+
+public class TextFieldActivity extends ButtonActivity implements OnClickListener{
+     
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        edittext.setOnKeyListener(new OnKeyListener() {
+			public boolean onKey(View arg0, int arg1, KeyEvent arg2) {
+				return false;
+			}
+        });       
+    }
+}
