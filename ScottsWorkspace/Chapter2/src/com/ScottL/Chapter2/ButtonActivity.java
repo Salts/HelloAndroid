0216175ca662aa1d798eb30608c7d809d1d95b3c
Scott L <schott12521@gmail.com>
2012-05-28 23:00:01 -0400

    Bored off of School

--- ScottsWorkspace/Chapter2/src/com/ScottL/Chapter2/ButtonActivity.java
+++ ScottsWorkspace/Chapter2/src/com/ScottL/Chapter2/ButtonActivity.java
@@ -3,8 +3,10 @@ package com.ScottL.Chapter2;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -26,13 +28,22 @@ public class ButtonActivity extends Activity implements OnClickListener{
 			public void onClick(View v) {
 				Toast.makeText(getBaseContext() , edittext.getText(), 250).show();
 			}
-		});
+		}); 
+    edittext.setOnKeyListener(new OnKeyListener() {
+    	public boolean onKey(View v, int keyCode, KeyEvent event) {
+    		if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+    	            (keyCode == KeyEvent.KEYCODE_ENTER)) {
+    				Toast.makeText(getBaseContext() , edittext.getText(), 250).show();
+    				return true;
+    		}
+    				return false;
+    	}
+	});
     }
 
 	public void onClick(View v) {
 		Intent intent = new Intent(ButtonActivity.this, TextFieldActivity.class);
 		intent.putExtra("text", edittext.getText().toString());
 		startActivity(intent);
-		
 	}
 }
--- ScottsWorkspace/Chapter2/src/com/ScottL/Chapter2/TextFieldActivity.java
+++ ScottsWorkspace/Chapter2/src/com/ScottL/Chapter2/TextFieldActivity.java
@@ -18,5 +18,6 @@ public class TextFieldActivity extends ButtonActivity implements OnClickListener
 				return false;
 			}
         });       
+
     }
 }
